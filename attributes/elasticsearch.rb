9a20099938396b7a31f28278ebb9f6c5d04b7af7
Steven Gonzales <stevendgonzales@gmail.com>
2014-02-06 11:20:53 -0600

    update for elasticsearch

---
+++ attributes/elasticsearch.rb
@@ -0,0 +1,4 @@
+default[:meetme_newrelic_plugin][:elasticsearch][:clustername] = "es_cluster"
+default[:meetme_newrelic_plugin][:elasticsearch][:host] = "localhost"
+default[:meetme_newrelic_plugin][:elasticsearch][:port] = 9200
+default[:meetme_newrelic_plugin][:elasticsearch][:scheme] = "http"
